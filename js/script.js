ac4d3ffd08fec73858ae015497e83c9c392eef17
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-16 17:22:59 +0200

    Bonus 2 v2

--- js/script.js
+++ js/script.js
@@ -1,39 +1,37 @@
 let varPrinted;
-const wrapperRow=document.getElementById(`wrapper-row`)
+const wrapperRow = document.getElementById(`wrapper-row`)
 
-for(i=1;i<=100;i++){
-    let newBox=document.createElement('div');
-    if(i%3===0 && i%5===0) //verifico fin da subito se il numero è divisibile per 3 e 5
+for (i = 1; i <= 100; i++) {
+    let newBox = document.createElement('div');
+    if (i % 3 === 0 && i % 5 === 0) //verifico fin da subito se il numero è divisibile per 3 e 5
     {
-        varPrinted=`FizzBuzz`;          //inserisco la parola FizzBuzz nel box
+        varPrinted = `FizzBuzz`;          //inserisco la parola FizzBuzz nel box
         newBox.classList.add('red');    //inserisco classe a sfondo rosso
     }
-    else{                   
-        if(i%5===0)         //verifico se è solo divisibile per 5
-        {
-            varPrinted=`Buzz`;              //inserisco la parola Buzz nel box
-            newBox.classList.add('yellow'); //inserisco classe a sfondo giallo
-        }
-    
-        else if(i%3===0)     //verifico se è solo divisibile per 3 
-        {
-            varPrinted=`Fizz`;              //inserisco la parola Fizz nel box
-            newBox.classList.add('blue');   //inserisco classe a sfondo blu
-        }
-    
-        else{               //non è divisibile ne per 3 e ne per 5, stampo il contatore
-            varPrinted=i;
-        }
+    else if (i % 3 !== 0 && i % 5 === 0)         //verifico se è solo divisibile solo per 5
+    {
+        varPrinted = `Buzz`;              //inserisco la parola Buzz nel box
+        newBox.classList.add('yellow'); //inserisco classe a sfondo giallo
     }
-    //console.log(varPrinted);
 
-    //ogni box sarà 2/12 largo rispetto al container
-    newBox.classList.add('col-2');
+    else if (i % 5 !== 0 && i % 3 === 0)     //verifico se è solo divisibile solo per 3 
+    {
+        varPrinted = `Fizz`;              //inserisco la parola Fizz nel box
+        newBox.classList.add('blue');   //inserisco classe a sfondo blu
+    }
 
-    //inserisco il valore all'interno del box
-    newBox.innerHTML = varPrinted;
+    else {               //non è divisibile ne per 3 e ne per 5, stampo il contatore
+        varPrinted = i;
+    }
 
-    //introduco nella row una nuova col-2
-    wrapperRow.append(newBox);
+//console.log(varPrinted);
 
+//ogni box sarà 2/12 largo rispetto al container
+newBox.classList.add('col-2');
+
+//inserisco il valore all'interno del box
+newBox.innerHTML = varPrinted;
+
+//introduco nella row una nuova col-2
+wrapperRow.append(newBox);
 }
