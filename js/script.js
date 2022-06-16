a72c15ae3f66851904ccf5848dc8173e77ec6e79
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-16 17:27:06 +0200

    Bonus 2 v3

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ for (i = 1; i <= 100; i++) {
         varPrinted = `FizzBuzz`;          //inserisco la parola FizzBuzz nel box
         newBox.classList.add('red');    //inserisco classe a sfondo rosso
     }
-    else if (i % 3 !== 0 && i % 5 === 0)         //verifico se è solo divisibile solo per 5
+    else if (i % 5 === 0)         //verifico se è solo divisibile solo per 5
     {
         varPrinted = `Buzz`;              //inserisco la parola Buzz nel box
         newBox.classList.add('yellow'); //inserisco classe a sfondo giallo
     }
 
-    else if (i % 5 !== 0 && i % 3 === 0)     //verifico se è solo divisibile solo per 3 
+    else if (i % 3 === 0)     //verifico se è solo divisibile solo per 3 
     {
         varPrinted = `Fizz`;              //inserisco la parola Fizz nel box
         newBox.classList.add('blue');   //inserisco classe a sfondo blu
